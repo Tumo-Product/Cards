c0428836c8e41137cbbc94f8d5d01887c9f8a3cc
varazdat-melkonyan <varazdat.melkonyan@tumo.org>
2021-12-10 12:31:16 +0400

    Update flow.js

--- scripts/flow.js
+++ scripts/flow.js
@@ -48,6 +48,9 @@ function readMore() {
         if ($(`#${set.index} .title`).css("height") > "50px") {
             $(`#${set.index} .text`).css("top", "130px")
         }
+        else if ($(`#${set.index} .title`).css("height") < "50px" && set.data[set.index].text.length > 650) {
+            $(`#${set.index} .text`).css("top", "120px")
+        }
         $(`#${set.index} .text`).css("max-height","300px");
         $(`#${set.index} .text`).css("height","auto");
         $(`#${set.index} .text`).css("overflow","auto");
